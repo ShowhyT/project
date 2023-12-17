092782165647b85065f359b1316d8e38de32b0bf
ShowhyT <mirantuten@ya.ru>
2023-12-17 19:33:04 +0300

    Новый дизайн и тест JS
    Co-authored-by: ShowhyT <109768004+ShowhyT@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let BtnMenu = document.getElementById("BtnMenu");
+let menu = document.createElement("noscript");
+
+BtnMenu.addEventListener('click',()=>{
+    window.alert("Это будет меню, надеюсь смогу его освоить|It`ll be a  menu, maybe i  develop it!")
+})
