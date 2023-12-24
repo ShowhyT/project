aa4f057ebd5509995ab4d11a6eab44803028655d
ShowhyT <109768004+ShowhyT@users.noreply.github.com>
2023-12-24 12:45:07 +0000

    test

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ let menu = document.createElement("noscript");
 
 BtnMenu.addEventListener('click',()=>{
     window.alert("Это будет меню, надеюсь смогу его освоить|It`ll be a  menu, maybe i  develop it!")
+    console.count(BtnMenu)
 })
